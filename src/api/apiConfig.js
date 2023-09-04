f9f307fd781615639d8f1ad5bc118dfd5eaf5c58
Eduardo <eduardorireyes2024@gmail.com>
2023-09-03 21:38:35 -0600

    [Update/Add] Nuevo manejo de varibles de entorno production/development

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,7 +1,24 @@
+// Configuracion para usar variables de entorno con vite
 const apiConfiguration = async () => {
-    const response = await fetch('../../config.json');
-    const config = await response.json();
+    await new Promise((resolve) => setTimeout(resolve), 100);
+
+    const config = {
+        API_KEY: import.meta.env.VITE_API_KEY,
+        URL_MOVIE: import.meta.env.VITE_URL_MOVIE,
+        URL_SEARCH: import.meta.env.VITE_URL_SEARCH,
+        URL_TRENDS: import.meta.env.VITE_URL_TRENDS,
+        URL_GENRES: import.meta.env.VITE_URL_GENRES,
+        URL_DISCOVER: import.meta.env.VITE_URL_DISCOVER,
+    }
+
     return config;
 }
 
 export { apiConfiguration }
+
+
+// const apiConfiguration = async () => {
+//     const response = await fetch('../../config.json');
+//     const config = await response.json();
+//     return config;
+// }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],  
+// eslint-disable-next-line no-unused-vars
+export default defineConfig(({command, mode}) => {
+  
+  //Cargar varibles de entorno.
+  // eslint-disable-next-line no-undef
+  loadEnv(mode, process.cwd())
+ 
+  if(mode === 'build'){
+    console.log('Mode Production');
+  }else if(mode === 'development'){
+    console.log('Mode Development');
+  }else{
+    console.log('Mode Preview');
+  }
+
+  return {
+    plugins: [react()], 
+  }
 })
